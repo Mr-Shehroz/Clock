16c2930b994ad71a509c9bfd8770215f38db1058
Mr-Shehroz <mr.shehroz.khan2006>
2025-01-15 14:40:11 +0500

    Add clock

---
+++ Clock.js
@@ -0,0 +1,16 @@
+let time = document.querySelector(".time")
+
+
+function updateClock() {
+    let now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    let ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    time.innerHTML = hours + ':' + minutes + ':' + seconds + ' ' + ampm;
+}
+setInterval(updateClock, 1000);
